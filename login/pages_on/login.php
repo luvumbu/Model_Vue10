2f561b8f141f7de32e164daa3bafc05de5c87590
luvumbu <luvumbu.n@gmail.com>
2023-11-02 18:11:31 +0100

    ajout de la fonction image sans projet

--- login/pages_on/login.php
+++ login/pages_on/login.php
@@ -447,6 +447,18 @@ ok.push(); // envoie l'information au code pkp
 		 
 }
 
+function add_img2(_this) {
+		var ok = new Information("pages_on/download_img/name_projet.php"); // création de la classe 
+		ok.add("name_projet", ""); // ajout de l'information pour lenvoi 
+		console.log(ok.info()); // demande l'information dans le tableau
+		ok.push(); // envoie l'information au code pkp 
+
+
+		 window.location.replace("pages_on/download_img/index.php");
+
+		 
+}
+
 function onclick_add_element (_this){
 	 
 	console.log(recherche_elements(_this," ")) ; 
