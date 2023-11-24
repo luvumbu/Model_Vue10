dd73832080ccaf50e0526df553290d56a4d3301d
luvumbu <luvumbu.n@gmail.com>
2023-11-24 22:17:05 +0100

    scroolbar ok

--- login/blog/pages.php
+++ login/blog/pages.php
@@ -519,6 +519,27 @@ text-shadow:1px 1px black;
     margin-bottom:60px; 
 
 }
+
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: red; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #b30000; 
+}
 </style>
 
 
